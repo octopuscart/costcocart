5457c9e54b547d4d29a2bdf7183e111292f4fa50
dev <dev@develop.com>
2019-05-16 13:34:31 +0530

    cahnges

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Shop';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['stylingTips'] = 'Shop/stylingTips';
+
+$route['stylingTipsTag'] = 'Shop/stylingTipsTag';
+$route['styleTips/(:any)/(:any)'] = 'Shop/styleTipsDetails/$1/$2';
